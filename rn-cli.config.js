171a121067c85c553763524f56570cb50ea94bef
葛辉 <terencegehui@yahoo.com>
2018-03-14 13:17:23 +0800

    move mobile componnets to shared

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -3,6 +3,6 @@ module.exports = {
     return require.resolve('react-native-typescript-transformer')
   },
   getSourceExts() {
-    return ['ts', 'mobile.ts', 'tsx', 'mobile.tsx']
+    return ['js', 'mobile.js', 'ts', 'mobile.ts', 'tsx', 'mobile.tsx']
   }
 }
