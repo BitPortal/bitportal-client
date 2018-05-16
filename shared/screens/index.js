7874c54005210112bdf5540806ce2bd4626e7688
Young Leo <liuyang_hubei@163.com>
2018-05-16 17:41:48 +0800

    contact list && add contact

--- shared/screens/index.js
+++ shared/screens/index.js
@@ -24,12 +24,14 @@ import PrivateKeyCreation from 'screens/Account/PrivateKeyCreation'
 
 import Discovery          from 'screens/Discovery'
 
-import Profile            from 'screens/Profile'
 import About              from 'screens/Profile/About'
+import Profile            from 'screens/Profile'
+import Contacts           from 'screens/Profile/Contacts'
 import Settings           from 'screens/Profile/Settings'
 import Languages          from 'screens/Profile/Languages'
 import Currencies         from 'screens/Profile/Currencies'
 import AccountList        from 'screens/Profile/AccountList'
+import CreateContact      from 'screens/Profile/CreateContact'
 import ResetPassword      from 'screens/Profile/ResetPassword'
 import AccountManager     from 'screens/Profile/AccountManager'
 import ExportEntrance     from 'screens/Profile/ExportEntrance'
@@ -57,10 +59,12 @@ export const registerScreens = (store, provider) => {
   
   Navigation.registerComponent('BitPortal.About',              () => About,              store, provider)
   Navigation.registerComponent('BitPortal.Profile',            () => Profile,            store, provider)
+  Navigation.registerComponent('BitPortal.Contacts',           () => Contacts,              store, provider)
   Navigation.registerComponent('BitPortal.Settings',           () => Settings,           store, provider)
   Navigation.registerComponent('BitPortal.Languages',          () => Languages,          store, provider)
   Navigation.registerComponent('BitPortal.Currencies',         () => Currencies,         store, provider)
   Navigation.registerComponent('BitPortal.AccountList',        () => AccountList,        store, provider)
+  Navigation.registerComponent('BitPortal.CreateContact',      () => CreateContact,      store, provider)
   Navigation.registerComponent('BitPortal.ResetPassword',      () => ResetPassword,      store, provider)
   Navigation.registerComponent('BitPortal.AccountManager',     () => AccountManager,     store, provider)
   Navigation.registerComponent('BitPortal.ExportEntrance',     () => ExportEntrance,     store, provider)
