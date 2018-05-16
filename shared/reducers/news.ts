437e8734e85c0f28273923ee082e27c71f59f2e1
liu9293 <mylock.kai@gmail.com>
2018-05-17 02:19:56 +0800

    delete useless console.log

--- shared/reducers/news.ts
+++ shared/reducers/news.ts
@@ -21,8 +21,6 @@ export default handleActions({
   },
   [actions.getNewsListSucceeded] (state, action) {
     const length = Immutable.fromJS(action.payload.data).size;
-    console.log('length: ', length)
-
     if (length === 0) {
       return state
         .set('nomore', true)
