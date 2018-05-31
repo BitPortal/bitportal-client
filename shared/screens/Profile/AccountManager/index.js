c7ba003f76a841d983a0205ffe45dd58d60706c8
Terence Ge <Terencegehui@yahoo.com>
2018-05-31 17:03:28 +0800

    get balance

--- shared/screens/Profile/AccountManager/index.js
+++ shared/screens/Profile/AccountManager/index.js
@@ -124,7 +124,7 @@ export default class AccountList extends BaseScreen {
               showsVerticalScrollIndicator={false}
               contentContainerStyle={{ alignItems: 'center', paddingBottom: 20 }}
             >
-              <TotalAssetsCard totalAssets={0} accountName={eosAccountName} disabled={true} />
+              {/* <TotalAssetsCard totalAssets={0} accountName={eosAccountName} disabled={true} /> */}
               <SettingItem leftItemTitle={<FormattedMessage id="act_sec_title_change" />} onPress={() => this.resetPassword()} extraStyle={{ marginTop: 10 }} />
               <SettingItem leftItemTitle={<FormattedMessage id="act_sec_title_export" />} onPress={() => this.exportAccount()} extraStyle={{ marginTop: 10 }} />
               <SettingItem leftItemTitle={<FormattedMessage id="act_sec_title_logout" />} onPress={() => this.logout()} extraStyle={{ marginTop: 10 }} />
