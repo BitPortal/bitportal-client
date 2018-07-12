e0f0457ef6302509c1825d1507ee39b807e5d589
Young Leo <liuyang_hubei@163.com>
2018-07-12 15:02:45 +0800

    analytics

--- android/app/src/main/java/com/bitportal/MainActivity.java
+++ android/app/src/main/java/com/bitportal/MainActivity.java
@@ -3,10 +3,13 @@ import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.reactnativenavigation.NavigationActivity;
+import com.umeng.analytics.MobclickAgent;
 
 public class MainActivity extends NavigationActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+      MobclickAgent.setSessionContinueMillis(1000);
+      MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_DUM_NORMAL);
       SplashScreen.show(this, true);
       super.onCreate(savedInstanceState);
   }
@@ -14,10 +17,12 @@ public class MainActivity extends NavigationActivity {
   @Override
   public void onResume() {
       super.onResume();
+      MobclickAgent.onResume(this);
   }
 
   @Override
   protected void onPause() {
       super.onPause();
+      MobclickAgent.onPause(this);
   }
 }
