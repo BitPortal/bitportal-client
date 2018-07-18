407a7835953bb25612bc5170b2e7c24a34130c70
Terence Ge <Terencegehui@yahoo.com>
2018-07-18 20:20:53 +0800

    split vender

--- webpack.config.js
+++ webpack.config.js
@@ -183,6 +183,25 @@ const browserConfig = {
     filename: ifProduction('scripts/bundle.js?v=[hash]', 'scripts/bundle.js'),
     publicPath: '/'
   },
+  optimization: {
+	splitChunks: {
+	  cacheGroups: {
+		commons: {
+		  chunks: 'initial',
+		  minChunks: 2,
+		  maxInitialRequests: 5,
+		  minSize: 0
+		},
+		vendor: {
+		  test: /node_modules/,
+		  chunks: 'initial',
+		  name: 'vendor',
+		  priority: 10,
+		  enforce: true
+		}
+	  }
+	}
+  },
   plugins: removeEmpty([
     ...baseConfig.plugins,
     ifNotProduction(new webpack.HotModuleReplacementPlugin()),
