f938a3338760226981057247de4bca4d09b39aa2
Young Leo <liuyang_hubei@163.com>
2018-07-24 13:06:20 +0800

    hidden single eos display

--- shared/screens/Assets/BalanceList.jsx
+++ shared/screens/Assets/BalanceList.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Text, View, TouchableHighlight } from 'react-native'
 import Colors from 'resources/colors'
 import { FormattedNumber } from 'react-intl'
+import { connect } from 'react-redux'
 import CurrencyText from 'components/CurrencyText'
 import styles from './styles'
 
-const ListItem = ({ item, onPress, eosPrice }) => (
+const ListItem = ({ item, onPress, eosPrice, isAssetHidden }) => (
   <TouchableHighlight underlayColor={Colors.hoverColor} style={styles.listContainer} onPress={() => onPress(item)}>
     <View style={[styles.listContainer, styles.between, { paddingHorizontal: 32, backgroundColor: Colors.minorThemeColor }]}>
       <View style={{ flexDirection: 'row' }}>
@@ -13,25 +14,64 @@ const ListItem = ({ item, onPress, eosPrice }) => (
       </View>
       <View>
         <Text style={[styles.text20, { alignSelf: 'flex-end' }]}>
-          <FormattedNumber
-            value={item.get('balance')}
-            maximumFractionDigits={4}
-            minimumFractionDigits={4}
-          />
+          {
+            isAssetHidden ?
+            '******'
+            :
+            <FormattedNumber
+              value={item.get('balance')}
+              maximumFractionDigits={4}
+              minimumFractionDigits={4}
+            />
+          }
+          
         </Text>
-        <Text style={[styles.text14, { alignSelf: 'flex-end', color: Colors.textColor_149_149_149 }]}>
-          ≈
-          <CurrencyText
-            value={+item.get('balance') * +eosPrice}
-            maximumFractionDigits={2}
-            minimumFractionDigits={2}
-          />
-        </Text>
+        {
+          isAssetHidden ?
+          <Text style={[styles.text14, { alignSelf: 'flex-end', color: Colors.textColor_149_149_149 }]}>
+           ******
+          </Text>
+          :
+          <Text style={[styles.text14, { alignSelf: 'flex-end', color: Colors.textColor_149_149_149 }]}>
+            ≈
+            <CurrencyText
+              value={+item.get('balance') * +eosPrice}
+              maximumFractionDigits={2}
+              minimumFractionDigits={2}
+            />
+          </Text>
+        }
       </View>
     </View>
   </TouchableHighlight>
 )
 
-export default ({ data, onPress, eosPrice }) => (
-  <View>{data.map((item, index) => (<ListItem key={index} item={item} eosPrice={eosPrice} onPress={e => onPress(e)} />))}</View>
+@connect(
+  state => ({
+    locale: state.intl.get('locale'),
+    isAssetHidden: state.eosAccount.get('isAssetHidden')
+  })
 )
+
+export default class BalanceList extends Component {
+
+  render() {
+    const { data, onPress, eosPrice, isAssetHidden } = this.props
+    return (
+      <View>
+        {
+          data.map((item, index) => (
+            <ListItem 
+              key={index} 
+              item={item} 
+              isAssetHidden={isAssetHidden}
+              eosPrice={eosPrice} 
+              onPress={e => onPress(e)} 
+              />
+            )
+          )
+        }
+      </View>
+    )
+  }
+} 
