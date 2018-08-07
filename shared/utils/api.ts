2310a27af5802bd8c28101c15ef717f4dde6c36c
Terence Ge <Terencegehui@yahoo.com>
2018-08-07 21:50:39 +0800

    fix lint

--- shared/utils/api.ts
+++ shared/utils/api.ts
@@ -45,7 +45,7 @@ export const fetchBase = async (
 
   return fetch(url, fetchOptions).then((res: any) => {
     if (!res.ok) {
-      return res.json().then((e: any) => Promise.reject({ message: e.error }))
+      return res.json().then((e: any) => Promise.reject({ message: e }))
     }
 
     const contentType = res.headers.get('content-type')
