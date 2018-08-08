5161785fba50b68938eb4fd05421986b8a4d1972
Neil <neilasjoseph@gmail.com>
2018-08-08 15:10:33 +0800

    Merge branch 'master' into nMarket-token-info & last minute fixes

--- shared/screens/Market/Quotes.jsx
+++ shared/screens/Market/Quotes.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
-import Colors from 'resources/colors'
-import styles from './styles'
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import Colors from 'resources/colors';
+import styles from './styles';
 
 export const Quotes = ({ quoteList, quote, onPress }) => (
   <View style={styles.quoteContainer}>
-    {
-      quoteList.map(item => (
-        <TouchableOpacity
-          key={item}
-          onPress={() => { onPress(item) }}
-          style={[styles.center, {
+    {quoteList.map(item => (
+      <TouchableOpacity
+        key={item}
+        onPress={() => {
+          onPress(item);
+        }}
+        style={[
+          styles.center,
+          {
             flex: 1,
             borderBottomColor: Colors.borderColor_89_185_226,
-            borderBottomWidth: quote === item ? StyleSheet.hairlineWidth : 0
-          }]}
-        >
-          <Text style={[styles.text16, { color: Colors.textColor_255_255_238 }]}>{item}</Text>
-        </TouchableOpacity>
-      ))
-    }
+            borderBottomWidth: quote === item ? StyleSheet.hairlineWidth * 6 : 0
+          }
+        ]}
+      >
+        <Text style={[styles.text16, { color: Colors.textColor_255_255_238 }]}>
+          {item}
+        </Text>
+      </TouchableOpacity>
+    ))}
   </View>
-)
+);
