e797df6ce8f2462f36b4656c7bc3d8d241292f8b
Terence Ge <Terencegehui@yahoo.com>
2018-08-10 11:38:15 +0800

    fix

--- shared/constants/chain.ts
+++ shared/constants/chain.ts
@@ -12,8 +12,7 @@ export const EOS_TESTNET_NODES = [
 ]
 
 export const EOS_NODES = [
-  ...EOS_MAINNET_NODES,
-  ...EOS_TESTNET_NODES
+  ...EOS_MAINNET_NODES
 ]
 
 export const EOS_MAINNET_CHAIN_ID = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
