5f700cb8ee0522dd834158406830e34638ff7752
Young Leo <liuyang_hubei@163.com>
2018-08-22 11:11:00 +0800

    modify memory price link

--- shared/screens/Resources/Memory/index.jsx
+++ shared/screens/Resources/Memory/index.jsx
@@ -45,11 +45,12 @@ export default class Memory extends Component {
   }
 
   checkRamPrice = () => {
-    const uri = 'https://eos.feexplorer.io/'
+    const uri = 'https://eosmonitor.io/'
     Navigation.push(this.props.componentId, {
       component: {
         name: 'BitPortal.BPWebView',
         passProps: {
+          title: uri,
           needLinking: true,
           uri
         }
