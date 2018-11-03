8bae9742d0d6441341cb65a4104dc9ab7a9659dd
Neil <neilasjoseph@gmail.com>
2018-11-03 12:37:11 +0000

    new logic token details as backend null values have changed

--- shared/screens/Market/Overview/TokenPage/TokenDetails/index.jsx
+++ shared/screens/Market/Overview/TokenPage/TokenDetails/index.jsx
@@ -22,7 +22,6 @@ import styles from './styles'
 export default class TokenDetails extends Component {
   render() {
     const { locale, token, loading } = this.props
-    console.log('tokenn', token.toJS())
     return loading ? (
       <View>
         <DetailPanel>
