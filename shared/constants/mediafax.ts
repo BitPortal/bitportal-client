dce039ae15c87f2c731a1d819558dda1b2365474
starit <janyucheng@gmail.com>
2019-05-07 01:21:33 +0800

    change link

--- shared/constants/mediafax.ts
+++ shared/constants/mediafax.ts
@@ -19,7 +19,7 @@ export const MediafaxUrls = {
   WechatService: 'bitportal_jamie',
   Telegram: 'https://t.me/BitPortal_official_EN',
   Kakao: 'https://open.kakao.com/o/gWAXZTO',
-  Twitter: 'https://twitter.com/bit_portal',
+  Twitter: 'https://twitter.com/BitPortal_IO',
   Facebook: 'https://www.facebook.com/BitPortal',
   Weibo: 'https://weibo.com/u/6541262254',
   WechatOfficial: '@BitPortal币通'
