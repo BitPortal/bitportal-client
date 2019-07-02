a2444ff7e1cd1e26805fe5ad6893815dd4bd89ac
starit <janyucheng@gmail.com>
2019-07-02 09:44:24 +0800

    improve ui in transaction detail page

--- shared/screens/Wallet/TransferAsset/index.ios.jsx
+++ shared/screens/Wallet/TransferAsset/index.ios.jsx
@@ -513,7 +513,7 @@ export default class TransferAsset extends Component {
   }
 
   submit = (data) => {
-    const { transferWallet, formSyncWarnings, transferAsset, assetBalance, walletBalance } = this.props
+    const { intl, transferWallet, formSyncWarnings, transferAsset, assetBalance, walletBalance } = this.props
     const balance = (transferAsset && transferAsset.contract) ? assetBalance : walletBalance
 
     if (typeof formSyncWarnings === 'object') {
@@ -523,7 +523,7 @@ export default class TransferAsset extends Component {
           warning,
           '',
           [
-            { text: '确定', onPress: () => console.log('OK Pressed') }
+            { text: intl.formatMessage({ id: 'alert_button_confirm' }), onPress: () => console.log('OK Pressed') }
           ]
         )
         return
@@ -532,16 +532,16 @@ export default class TransferAsset extends Component {
     // Keyboard.dismiss()
 
     Alert.prompt(
-      '请输入钱包密码',
+      intl.formatMessage({ id: 'alert_input_wallet_password' }),
       null,
       [
         {
-          text: '取消',
+          text: intl.formatMessage({ id: 'alert_button_cancel' }),
           onPress: () => console.log('Cancel Pressed'),
           style: 'cancel'
         },
         {
-          text: '确认',
+          text: intl.formatMessage({ id: 'alert_button_confirm' }),
           onPress: password => this.props.actions.transfer.requested({
             ...data,
             password,
@@ -835,7 +835,6 @@ export default class TransferAsset extends Component {
     const useGasPrice = this.state.useGasPrice || this.state.initialGwei || this.state.ethGasPrice || 4.00
 
     return (
-      <SafeAreaView style={{flex: 1, backgroundColor: 'white' }}>
       <ScrollView
         style={[styles.container, { backgroundColor: 'white' }]}
         showsVerticalScrollIndicator={false}
@@ -844,7 +843,7 @@ export default class TransferAsset extends Component {
       >
           <View style={{ flex: 1, width: '100%', alignItems: 'center', borderTopWidth: 0, borderBottomWidth: 0, borderColor: '#C8C7CC', justifyContent: "flex-end" }}>
           <Field
-            label="地址"
+            label={intl.formatMessage({ id: 'send_input_label_to_address' })}
             placeholder={`请输入${symbol}${chain === 'EOS' ? '账户名' : '地址'}`}
             name="toAddress"
             fieldName="toAddress"
@@ -861,7 +860,7 @@ export default class TransferAsset extends Component {
             showMemo={showMemo}
           />
           <Field
-            label="备注"
+            label={intl.formatMessage({ id: 'send_input_label_send_memo' })}
             placeholder={(selectedContact && !!selectedContact.memo) ? `选填，默认为: ${selectedContact.memo}` : '添加备注 (选填)'}
             name="memo"
             fieldName="memo"
@@ -872,7 +871,7 @@ export default class TransferAsset extends Component {
             separator
           />
           <Field
-            label="支付帐号"
+            label={intl.formatMessage({ id: 'send_input_label_payer_account' })}
             placeholder=""
             name="card"
             fieldName="card"
@@ -886,7 +885,7 @@ export default class TransferAsset extends Component {
             separator
           />
           <Field
-            label="金额"
+            label={intl.formatMessage({ id: 'send_input_label_send_amount' })}
             placeholder="00.00"
             name="amount"
             fieldName="amount"
@@ -899,7 +898,7 @@ export default class TransferAsset extends Component {
           {showMinnerFee && chain === 'BITCOIN' && <View style={{ width: '100%', paddingLeft: 16, paddingRight: 16 }}>
             <View style={{ width: '100%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between' }}>
               <View style={{ flexDirection: 'row', height: 50, alignItems: 'center' }}>
-                <Text style={{ fontSize: 15 }}>矿工费</Text>
+                <Text style={{ fontSize: 15 }}>{intl.formatMessage({id: 'send_label_mining_fee'})}</Text>
                 <TouchableHighlight
                   underlayColor="white"
                   activeOpacity={0.42}
@@ -948,7 +947,7 @@ export default class TransferAsset extends Component {
           {showMinnerFee && chain === 'ETHEREUM' && <View style={{ width: '100%', paddingLeft: 16, paddingRight: 16 }}>
             <View style={{ width: '100%', alignItems: 'center', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
               <View style={{ flexDirection: 'row', height: 50, alignItems: 'center' }}>
-                <Text style={{ fontSize: 15 }}>矿工费</Text>
+                <Text style={{ fontSize: 15 }}>{intl.formatMessage({ id: 'send_input_label_mining_fee' })}</Text>
                 <TouchableHighlight
                   underlayColor="white"
                   activeOpacity={0.42}
@@ -998,7 +997,7 @@ export default class TransferAsset extends Component {
             <View style={{ position: 'absolute', height: 0.5, top: 0, right: 16, left: 16, backgroundColor: '#C8C7CC' }} />
             <View style={{ width: '100%', height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingLeft: 16, paddingRight: 16 }}>
               <View style={{ height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
-                <Text style={{ fontSize: 15 }}>高级设置</Text>
+                <Text style={{ fontSize: 15 }}>{intl.formatMessage({ id: 'send_toggle_advanced_setting' })}</Text>
                 {/* <TouchableHighlight
                     underlayColor="white"
                     activeOpacity={0.42}
@@ -1028,7 +1027,7 @@ export default class TransferAsset extends Component {
             </View>
             {this.state.showOPReturn && <Field
               label="opreturn"
-              placeholder="输入OP_RETURN 16进制Hex"
+              placeholder={intl.formatMessage({ id: 'send_input_placeholder_opreturn_hex' })}
               name="opreturn"
               fieldName="opreturn"
               component={TextField}
@@ -1045,10 +1044,9 @@ export default class TransferAsset extends Component {
               onPress={this.props.handleSubmit(this.submit)}
               style={{ backgroundColor: '#007AFF', borderRadius: 10, height: 50, alignItems: 'center', justifyContent: 'center' }}
             >
-              <Text style={{ textAlign: 'center', color: 'white', fontSize: 17 }}>发送</Text>
+              <Text style={{ textAlign: 'center', color: 'white', fontSize: 17 }}>{intl.formatMessage({ id: 'send_button_send' })}</Text>
             </TouchableHighlight>
           </View>
-          <View style={{ flex: 1 }} />
         </View>
         <Modal
           isVisible={this.state.selectContact}
@@ -1115,12 +1113,11 @@ export default class TransferAsset extends Component {
           {loading && <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 14, alignItem: 'center', justifyContent: 'center', flexDirection: 'row' }}>
               <ActivityIndicator size="small" color="#000000" />
-              <Text style={{ fontSize: 17, marginLeft: 10, fontWeight: 'bold' }}>交易发送中...</Text>
+              <Text style={{ fontSize: 17, marginLeft: 10, fontWeight: 'bold' }}>{intl.formatMessage({ id: 'transfer_alert_sending_transaction' })}</Text>
             </View>
           </View>}
         </Modal>
       </ScrollView>
-      </SafeAreaView>
     )
   }
 }
